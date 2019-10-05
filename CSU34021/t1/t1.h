369ecb06b4b2309b4bb224cd1b98b5afcc1bc2e7
rwejiv <carlzzzlk@gmail.com>
2019-10-05 01:00:08 +0100

    fixed

--- CSU34021/t1/t1.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-extern "C" int g;							// global variable g
-extern "C" int _cdecl min(int, int, int);
-extern "C" int _cdecl p(int, int, int, int);
-extern "C" int _cdecl gcd(int, int);
