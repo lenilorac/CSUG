e3272825b8474da9922b4cf0087f8367f2b7fc0f
rwejiv <carlzzzlk@gmail.com>
2019-10-09 21:47:40 +0100

    added test cases

--- CSU33012/LCA/LCA.cpp
+++ CSU33012/LCA/LCA.cpp
@@ -78,10 +78,17 @@ int main()
 	root->left->right = newNode(5); 
 	root->right->left = newNode(6); 
 	root->right->right = newNode(7); 
+	root->left->left->left = newNode(8);
+	root->left->left->right = newNode(9);
+
+	cout << "LCA of a null tree = " << findLCA(NULL, 0, 0);	
 	cout << "LCA(4, 5) = " << findLCA(root, 4, 5); 
-	cout << "nLCA(4, 6) = " << findLCA(root, 4, 6); 
-	cout << "nLCA(3, 4) = " << findLCA(root, 3, 4); 
-	cout << "nLCA(2, 4) = " << findLCA(root, 2, 4); 
+	cout << "LCA(4, 6) = " << findLCA(root, 4, 6); 
+	cout << "LCA(3, 4) = " << findLCA(root, 3, 4); 
+	cout << "LCA(2, 4) = " << findLCA(root, 2, 4); 
+	cout << "LCA(9, 5) = " << findLCA(root, 9, 5);
+	cout << "LCA(7, 9) = " << findLCA(root, 7, 9);
+	cout << "LCA(4, 8) = " << findLCA(root, 4, 8);
 	return 0; 
 } 
 
