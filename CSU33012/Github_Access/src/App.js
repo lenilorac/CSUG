939ee83e3c1bc4cdd990a230610774ae6881ba33
rwejiv <carlzzzlk@gmail.com>
2019-11-28 20:25:07 +0000

    awaiting fixtures

--- CSU33012/Github_Access/src/App.js
+++ CSU33012/Github_Access/src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Heatmap from './Heatmap'
 import './App.css';
 import * as d3 from "d3";
+import {hexbin} from 'd3-hexbin';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {data:[]}
+    this.getData();
+    setInterval(()=>this.geData(), 1000);
+  }
+
+  async getFollowers(){
+    var username;
+    var api_link = 'https://api.github.com/users/rwejiv/followers';
+    const response = await fetch(api_link);
+    const data = await response.json();
+    for (var i = 0; i < data.lenth; i++){
+      username = data[i].login;
+    }
+    // fixture
+  }
+
   render() {
+    if (this.state.data.length === 0){
+      return null;
+    }
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Number of followers</h2>
+        <HeatMap data = {this.state.data}/>
       </div>
     );
   }
--- CSU33012/Github_Access/src/fudamentals.js
+++ CSU33012/Github_Access/src/fudamentals.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import {hexbin} from 'd3-hexbin';
+import * as d3 from d3;
+
+const heatMap = ({data}) => {
+  const margin = {top:20, bottom:20, right:20, left:50}, width = 1000, height = 400;
+  
+  const metaData = d3.nest().key(function(d){return d.key;})
+}
+
+/*
 async function getFollowers(){
   var username;
   var api_url = 'https://api.github.com/users/rwejiv/followers';
@@ -6,7 +17,7 @@ async function getFollowers(){
   for (var i = 0; i < data.length; i++){
       username = data[i].login;
   }
-}
+
 
 var save_file = require('file_save');
 
@@ -15,3 +26,4 @@ save_file.appendFile('data.json', function(err){
     console.log('New file created.');
 }
 );
+}*/
